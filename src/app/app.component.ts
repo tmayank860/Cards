3bb34df32cee6491b23131ddc2833746a2443b64
tmayank860 <tmayank860@gmail.com>
2021-02-07 18:30:23 +0530

    CardsApp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Cards';
+  posts=[
+    {
+      imageUrl:'tree.jpeg',
+      title:'Neat Tree',
+      userName:'nature',
+      content:'I saw this neat tree during the hike today.'
+    },
+    {
+      imageUrl:'mountain.jpeg',
+      title:'Snowy mountain ',
+      userName:'mountainlover',
+      content:'Here is a picture of snowy mountain.'
+    },
+    {
+      imageUrl:'biking.jpeg',
+      title:'Mountain biking',
+      userName:'biking1222',
+      content:'I did some biking today.'
+    }
+  ]
 }
